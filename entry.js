89a206b1ac1b9db981a03122b4b9129c583761a1
xubaoshi <948869516@qq.com>
2017-02-10 10:36:13 +0800

    add react on ie8

---
+++ entry.js
@@ -0,0 +1,21 @@
+require('es5-shim');
+require('es5-shim/es5-sham');
+require('console-polyfill');
+require('es6-promise');
+require('fetch-ie8');
+
+/**
+ * CANNOT use `import` to import `react` or `react-dom`,
+ * because `import` will run `react` before `require('es5-shim')`.
+ */
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+const App = require('./App');
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('app')
+);
