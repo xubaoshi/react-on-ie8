0990d47a47715828dd1dda513a1a018d5541cc7d
xubaoshi <948869516@qq.com>
2017-02-15 09:49:32 +0800

    add react

--- components/cnode/List.jsx
+++ components/cnode/List.jsx
@@ -4,15 +4,15 @@ import { bindActionCreators } from "redux";
 
 
 var CnodeList = React.createClass({
-    componentDidMount:function(){
+    componentDidMount: function () {
         const {getTopics} = this.props
         getTopics()
     },
     render: function () {
         const {cnodes} = this.props
         var cnodeNodes = ''
-        if (cnodes && cnodes.length > 0) {
-            cnodeNodes = cnodes.map(function (cnode) {
+        if (cnodes.data && cnodes.data.length > 0) {
+            cnodeNodes = cnodes.data.map(function (cnode) {
                 return (
                     <span key={cnode.id}> {cnode.author_id}</span>
                 )
@@ -20,29 +20,30 @@ var CnodeList = React.createClass({
         }
         return (
             <div className="cnodeList" >
-                {cnodeNodes}
+                {cnodes.isFetching ? <span>fetching......</span> : cnodeNodes}
             </div>
         )
     }
 })
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    cnodes: state.rootReducer.cnodes
-  }
+    return {
+        cnodes: state.rootReducer.cnodes
+    }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-      getTopics:function(){
-        const url = 'https://cnodejs.org/api/v1/topics'
-        fetch(url)
-        .then((res) => res.json())
-        .then((res) => {
-            dispatch({type:'LIST_CNODE_REQUEST', res})
-        });
-      }
-  }
+    return {
+        getTopics: function () {
+            const url = 'https://cnodejs.org/api/v1/topics'
+            dispatch({ type: 'LIST_CNODE_REQUEST' })
+            fetch(url)
+                .then((res) => res.json())
+                .then((res) => {
+                    dispatch({ type: 'LIST_CNODE_REQUEST_SUCCESS', res })
+                });
+        }
+    }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(CnodeList)
--- redux/reducers/cnodes.js
+++ redux/reducers/cnodes.js
@@ -2,14 +2,15 @@ import * as Types from '../../constants/actionTypes';
 import * as Actions from '../actions';
 import objectAssign from 'object-assign';
 
-const initialState = [
-]
+const initialState = { isFetching: false, data: [] }
 
 export default function comments(state = initialState, action) {
     switch (action.type) {
         case Types.LIST_CNODE_REQUEST:
-            return action.res.data;
+            return objectAssign({}, state, { isFetching: true });
+        case Types.LIST_CNODE_REQUEST_SUCCESS:
+            return objectAssign({}, state, { data: action.res.data, isFetching: false })
         default:
-        return state;
+            return state;
     }
 }
